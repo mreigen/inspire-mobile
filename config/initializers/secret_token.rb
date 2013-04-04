744627419edca6b888699dc476c5516296a2aecb
Minh Reigen <mreigen@gmail.com>
2013-04-04 00:19:47 -0700

    init commit with rails scaffhold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InspireMobile::Application.config.secret_token = 'cc1cd8f105d7f361d2a1ac797bebca03f2e08bf27d3c22664b56cb3400bf096efd77e895bc804d6f8391c5c62bb568666a8a940aad1757ef99fa0c2ec81bbd0b'
